d2d8791959dc0de290de36b94759f6a8dc4e03a0
Georg Wolf <Georg.Wolf@BSLINZ2-IT.LOCAL>
2023-09-21 17:00:37 +0200

    testing menu

--- Page/Pages/menu.php
+++ Page/Pages/menu.php
@@ -1,20 +1,32 @@
 <?php
+function getLastMonday(){
 $today = date('N'); // Gibt den aktuellen Wochentag als Zahl (1 für Montag, 7 für Sonntag) zurück
 
-$germanDays = array(
-    1 => 'Montag',
-    2 => 'Dienstag',
-    3 => 'Mittwoch',
-    4 => 'Donnerstag',
-    5 => 'Freitag',
-    6 => 'Samstag',
-    7 => 'Sonntag'
-);
-
 if ($today === '1') {
-    $date = $germanDays[$today] . ' ' . date('Y.m.d');
+    return date('Y.m.d');
 } else {
-    $date = $germanDays[$today] . ' ' . date('Y.m.d', strtotime('last Monday'));
+    return date('Y.m.d', strtotime('last Monday'));
+}
+}
+
+$lastMonday = getLastMonday();
+function getDate($day){
+
+  $germanDays = array(
+      1 => 'Montag',
+      2 => 'Dienstag',
+      3 => 'Mittwoch',
+      4 => 'Donnerstag',
+      5 => 'Freitag',
+      6 => 'Samstag',
+      7 => 'Sonntag'
+  );
+
+  if ($day === '1') {
+      return $germanDays[$day] . ' ' . date('Y.m.d');
+  } else {
+      return $germanDays[$day] . ' ' . date('Y.m.d', strtotime('last Monday'));
+  }
 }
 ?>
 
