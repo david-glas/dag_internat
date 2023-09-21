1dd1fc2919944a69080466b73aec4809291890db
David Glas <david@glas.at>
2023-09-21 11:58:24 +0200

    new navbar

--- Page/index.php
+++ Page/index.php
@@ -1,5 +1,5 @@
 <?php
-include "Database/conn.php";
+ include "Database/conn.php"; 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,11 +11,8 @@ include "Database/conn.php";
   <link href="assets/css/bootstrap.min.css" rel="stylesheet">
   <script src="assets/js/bootstrap.bundle.min.js"></script>
 
-  <!-- Elements -->
-  <link href="assets/css/dashboard.css" rel="stylesheet">
-  <link href="assets/css/offcanvas-navbar.css" rel="stylesheet">
-  <script src="assets/js/dashboard.js"></script>
-  <script src="assets/js/offcanvas-navbar.js"></script>
+  <!-- Navbar -->
+  <link href="assets/css/navbars.css" rel="stylesheet">
 
   <title>Document</title>
 </head>
@@ -33,25 +30,13 @@ include "Database/conn.php";
         break;
       case 'menu':
         include "Pages/menu.php";
+        break;
       default:
-      include "Pages/landing.php";
+        include "Pages/landing.php";
+        break;
     }
   } else { include "Pages/landing.php"; }
 ?>
   
 </body>
 </html>
-
-<?php
-$user = new User();
-$food = new Food();
-$menu = new Menu();
-
-$date = date('2023-09-18');
-$menu->AddOrUpdMenuEntry(2, 2, $date);
-
-#$user->UpdateUser("0205965818", "Horst", "Geberle", "saufen", 2);
-#$result = $user->CheckLogin("0205965818", "saufen");
-#$result = $food->AddFood("Chicken", array(2, 2));
-#echo $result;
-?>
