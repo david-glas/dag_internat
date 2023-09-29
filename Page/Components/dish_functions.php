a0eaaa8db12ca3c7abf1d3b101e12b1227bfc16a
David Glas <david@glas.at>
2023-09-29 11:52:43 +0200

    dish dashboard update & delete

--- Page/Components/dish_functions.php
+++ Page/Components/dish_functions.php
@@ -2,23 +2,32 @@
 include "../Database/conn.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $test = $_POST;
-  $firstname = $_POST['firstname_input'];
-  $lastname = $_POST['lastname_input'];
-  $svnr = $_POST['svnr_input'];
-  $password = $_POST['password_input'];
-  $role_id = $_POST['role_select'];
-  $action = $_POST['action'];
+  if (array_key_exists('foodID_input', $_POST)) { $food_id = $_POST['foodID_input']; }
+  if (array_key_exists('foodName_input', $_POST)) { $food_name = $_POST['foodName_input']; }
+  if (array_key_exists('breakfast', $_POST)) { $breakfast = $_POST['breakfast']; }
+  if (array_key_exists('starter', $_POST)) { $starter = $_POST['starter']; }
+  if (array_key_exists('lunch', $_POST)) { $lunch = $_POST['lunch']; }
+  if (array_key_exists('dessert', $_POST)) { $dessert = $_POST['dessert']; }
+  if (array_key_exists('dinner', $_POST)) { $dinner = $_POST['dinner']; }
+  if (array_key_exists('action', $_POST)) { $action = $_POST['action']; }
+  
+  $meal_ids = array();
 
-  $user = new User();
+  if (isset($breakfast)) { $meal_ids[] = '1'; }
+  if (isset($starter)) { $meal_ids[] = '2'; }
+  if (isset($lunch)) { $meal_ids[] = '3'; $meal_ids[] = '4'; }
+  if (isset($dessert)) { $meal_ids[] = '5'; }
+  if (isset($dinner)) { $meal_ids[] = '6'; $meal_ids[] = '7'; }
+
+  $foodCon = new Food();
   if ($action == 'create') {
-    $user->AddUser($svnr, $firstname, $lastname, $password, $role_id);
+    $foodCon->AddFood($food_name, $meal_ids);
   }
   else if ($action == 'change') {
-    $user->UpdateUser($svnr, $firstname, $lastname, $password, $role_id);
+    $foodCon->UpdateFood($food_id, $food_name, $meal_ids);
   }
   else if ($action == 'delete') {
-    $user->DeleteUser($svnr);
+    $foodCon->DeleteFood($food_id);
   }
 }
   header("Location: ../index.php?page=dashboard&subpage=dishes");
--- Page/Pages/dishes.php
+++ Page/Pages/dishes.php
@@ -11,12 +11,12 @@
     <div class="container">
       <form class="form-floating" method="POST" action="Components/dish_functions.php">
         <div class="form-floating mb-2">
-          <input type="text" class="form-control" id="firstname_input" name="firstname_input">
+          <input type="text" class="form-control" id="foodName_input" name="foodName_input">
           <label for="firstname_input">Name</label>
         </div>
-        <?php fillMealChecks(array()) ?>
+        <?php fillMealChecks("") ?>
         <div class="form-floating mt-4">
-          <button type="submit" name="action" value="create" class="btn btn-success">Gericht anlegen</button>
+          <button type="submit" name="action" value="create" class="btn btn-success"><i class="bi bi-check-lg d-inline"></i> Gericht anlegen</button>
         </div>
       </form>
     </div>
@@ -92,32 +92,38 @@
                 <td>'. $food['name'] .'</td>
                 <td>'. $food['menu_names'] .'</td>
                 <td>
-                  <button type="button" class="btn btn-warning btn-sm float-end" id='. $timeOfDay . $food['food_id'] .'>
+                  <button type="button" class="btn btn-warning btn-sm float-end" data-bs-toggle="offcanvas" data-bs-target="#change'. $food['food_id']  .'" aria-controls="change'. $food['food_id']  .'" id="'. $food['food_id'] .'">
                     <i class="bi bi-gear-fill"></i><span>  Ändern</span>
                   </button>
                 </td>
               </tr>';
 
-        echo  '<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+        echo  '<div class="offcanvas offcanvas-end" tabindex="-1" id="change'. $food['food_id']  .'" aria-labelledby="change'. $food['food_id']  .'">
                 <div class="offcanvas-header">
-                  <h5 class="offcanvas-title" id="offcanvasRightLabel"><i class="bi bi-cup-hot-fill"></i>  Gericht hinzufügen</h5>
+                  <h5 class="offcanvas-title" id="offcanvasRightLabel"><i class="bi bi-gear-fill"></i>  Gericht ändern</h5>
                   <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div class="offcanvas-body">
                   <div class="container">
                     <form class="form-floating" method="POST" action="Components/dish_functions.php">
                       <div class="form-floating mb-2">
-                        <input type="text" class="form-control" id="firstname_input" name="firstname_input">
-                        <label for="firstname_input">Name</label>
+                        <input type="hidden" value="'. $food['food_id'] .'" class="form-control" id="foodID_input" name="foodID_input">
+                        <input type="text" value="'. $food['food_id'] .'" class="form-control" id="foodID_input_inputhidden" name="foodID_input_inputhidden" disabled readonly>
+                        <label for="foodID_input_inputhidden">Essensnummer</label>
+                      </div>  
+                    <div class="form-floating mb-2">
+                        <input type="text" value="'. $food['name'] .'" class="form-control" id="foodName_input" name="foodName_input">
+                        <label for="foodName_input">Name</label>
                       </div>';
                         fillMealChecks($food['meal_ids']);
         echo          '<div class="form-floating mt-4">
-                      <button type="submit" name="action" value="create" class="btn btn-success">Gericht anlegen</button>
-                    </div>
-                  </form>
+                        <button type="submit" name="action" value="change" class="btn btn-success me-1"><i class="bi bi-check-lg"></i>  Gericht ändern</button>
+                        <button type="submit" name="action" value="delete" class="btn btn-danger"><i class="bi bi-trash-fill"></i>  Gericht löschen</button>
+                      </div>
+                    </form>
+                  </div>
                 </div>
-              </div>
-            </div>';
+              </div>';
       }
     }
   }
@@ -127,17 +133,61 @@
     $menus = $foodCon->GetAllMeals();
 
     if ($menus != null) {
-      for($i=0; $i < count($menus); $i++) {
-        echo '<div class="form-check mb-1">
-                <input class="form-check-input" type="checkbox" name="meal'. $menus[$i]['meal_id'] .'" value="">
-                <label class="form-check-label" for="'. $menus[$i]['meal_id'] .'">'
-                  .$menus[$i]['type'].
-                '</label>
-              </div>';
-      }
+      echo '<div class="container mt-3">
+              <h6>Vormittag</h6>
+            </div>
+            <div class="form-check mb-1">
+              <input class="form-check-input" type="checkbox" name="breakfast" value="set" '. setChecked('1', $meal_ids) .'>
+              <label class="form-check-label" for="breakfast">
+                Frühstück
+              </label>
+            </div>
+            <div class="container mt-3">
+              <h6>Mittag</h6>
+            </div>
+            <div class="form-check mb-1">
+              <input class="form-check-input" type="checkbox" name="starter" value="set" '. setChecked('2', $meal_ids) .'>
+              <label class="form-check-label" for="starter">
+                Vorspeise
+              </label>
+            </div>
+            <div class="form-check mb-1">
+              <input class="form-check-input" type="checkbox" name="lunch" value="set" '. setChecked('3', $meal_ids) .'>
+              <label class="form-check-label" for="lunch">
+                Hauptspeise
+              </label>
+            </div>
+            <div class="form-check mb-1">
+              <input class="form-check-input" type="checkbox" name="dessert" value="set" '. setChecked('5', $meal_ids) .'>
+              <label class="form-check-label" for="desert">
+                Nachspeise
+              </label>
+            </div>
+            <div class="container mt-3">
+              <h6>Abends</h6>
+            </div>
+            <div class="form-check mb-1">
+              <input class="form-check-input" type="checkbox" name="dinner" value="set" '. setChecked('6', $meal_ids) .'>
+              <label class="form-check-label" for="dinner">
+                Abendessen
+              </label>
+            </div>';
     }
-
-    
-
-
   }
+  function setChecked($meal_id, $meal_ids) {
+    $test = stringContains($meal_ids, $meal_id);
+    if (stringContains($meal_ids, $meal_id)) {
+      return 'checked';
+    }
+  }
+  function stringContains($haystack, $needle) {
+    // Use strpos to find the position of $needle in $haystack
+    $position = strpos($haystack, $needle);
+    
+    // Check if $position is not false, indicating that $needle was found
+    if ($position !== false) {
+        return true;
+    } else {
+        return false;
+    }
+}
