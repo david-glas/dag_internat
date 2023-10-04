895e2a0a218d9c921013bbcd7d9a7b968954c3a1
David Glas <david@glas.at>
2023-10-04 11:35:41 +0200

    landing page login new

--- Page/Pages/landing.php
+++ Page/Pages/landing.php
@@ -1,22 +1,22 @@
-<div class="form-signin" style="margin:70px">
-  <form method="POST" action="Components/login.php">
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
-    <div class="form-floating">
-      <input type="svnr" name="svnr" class="form-control" id="floatingInput" placeholder="0000000000">
-      <label for="floatingInput">SVNR</label>
+<div class="container col-xl-10 col-xxl-8 px-4 py-5">
+    <div class="row align-items-center g-lg-5 py-5">
+      <div class="col-lg-7 text-center text-lg-start">
+        <h1 class="display-4 fw-bold lh-1 text-body-emphasis mb-3">Willkommen zur Essensanmeldung!</h1>
+        <p class="col-lg-10 fs-4">Bei uns haben Sie die Möglichkeit, aus einer vielfältigen Liste von Mahlzeiten auszuwählen. Unsere Köche bereiten täglich köstliche Gerichte zu, und Sie haben die Freiheit, Ihre Essensauswahl ganz nach Ihrem Geschmack zu gestalten.</p>
+      </div>
+      <div class="col-md-10 mx-auto col-lg-5">
+        <form class="p-4 p-md-5 border rounded-3 bg-body-tertiary" method="POST" action="Components/login.php">
+        <h4 class="mb-4">Anmelden</h4>
+          <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="floatingInput" placeholder="0000000000" name="svnr">
+            <label for="floatingInput">SV-Nummer</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="0000000000" name="password">
+            <label for="floatingPassword">Passwort</label>
+          </div>
+          <button class="w-100 btn btn-lg btn-warning" type="submit">Login</button>
+        </form>
+      </div>
     </div>
-    <div class="form-floating">
-      <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="T">
-      <label for="floatingPassword">Passwort</label>
-    </div>
-
-    <div class="checkbox mb-3">
-      <label>
-        <input type="checkbox" value="remember-me"> Remember me
-      </label>
-    </div>
-    <button class="w-100 btn btn-lg btn-warning" type="submit">Anmelden</button>
-    <p class="mt-5 mb-3 text-muted">© 2017–2021</p>
-  </form>
-</div>
+  </div>
