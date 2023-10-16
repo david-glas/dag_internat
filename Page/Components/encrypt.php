a7a2b1eed29531215b804a54d8c50df5f9226482
Georg WOLF <georg.wolf@students.bs-linz2.ac.at>
2023-10-16 09:24:02 +0200

    test

--- Page/Components/encrypt.php
+++ Page/Components/encrypt.php
@@ -1,45 +1,47 @@
 <?php
-    session_start();
-    $userId = $_SESSION["user"]["userid"];
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+session_start();
+$userId = $_SESSION["user"]["userid"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $requestData = json_decode(file_get_contents('php://input'));
-    // Retrieve form data
-    $action = $requestData->action;
+  $requestData = json_decode(file_get_contents('php://input'));
+  // Retrieve form data
+  $action = $requestData->action;
 
 
-    if ($action == "encrypt"){
-      $tod = $requestData->tod;
-      $date = $requestData->day;
-      $text = '{"userid":'. $userId .',"tod":'. $tod .',"date":'. $date .'}';
+  if ($action == "encrypt") {
+    $tod = $requestData->tod;
+    $date = $requestData->day;
+    $text = '{"userid":"' . $userId . '","tod":"' . $tod . '","date":"' . $date . '"}';
 
-      $test = encrypt_decrypt($action, $text);
+    $test = encrypt_decrypt($action, $text);
 
-      echo $test;
-    } else if ($action == "decrypt"){
-      $text = $requestData->text;
-      $test = encrypt_decrypt($action, $text);
-      echo $test;
-    }
-  };
+    echo $test;
+  } else if ($action == "decrypt") {
+    $text = $requestData->text;
+    $test = encrypt_decrypt($action, $text);
+    //$json = json_encode($test);
+    echo $test;
+  }
+}
+;
 
-  function encrypt_decrypt($action, $string) 
-    {
-        $output = false;
-        $encrypt_method = "AES-256-CBC";
-        $secret_key = 'xxxxxxxxxxxxxxxxxxxxxxxx';
-        $secret_iv = 'xxxxxxxxxxxxxxxxxxxxxxxxx';
-        // hash
-        $key = hash('sha256', $secret_key);    
-        // iv - encrypt method AES-256-CBC expects 16 bytes 
-        $iv = substr(hash('sha256', $secret_iv), 0, 16);
-        if ( $action == 'encrypt' ) {
-            $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
-            $output = base64_encode($output);
-        } else if( $action == 'decrypt' ) {
-            $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
-        }
-        return $output;
-    }
+function encrypt_decrypt($action, $string)
+{
+  $output = false;
+  $encrypt_method = "AES-256-CBC";
+  $secret_key = 'xxxxxxxxxxxxxxxxxxxxxxxx';
+  $secret_iv = 'xxxxxxxxxxxxxxxxxxxxxxxxx';
+  // hash
+  $key = hash('sha256', $secret_key);
+  // iv - encrypt method AES-256-CBC expects 16 bytes 
+  $iv = substr(hash('sha256', $secret_iv), 0, 16);
+  if ($action == 'encrypt') {
+    $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+    $output = base64_encode($output);
+  } else if ($action == 'decrypt') {
+    $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+  }
+  return $output;
+}
 
 ?>
--- Page/Pages/scan.php
+++ Page/Pages/scan.php
@@ -1,14 +1,15 @@
-<div class="container"> 
-        <h1>Scan QR Codes</h1> 
-        <div class="section"> 
-          <div id="my-qr-reader"> 
-          </div> 
-          <div class="modal fade" id="scanModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="container">
+    <h1>Scan QR Codes</h1>
+    <div class="section">
+        <div id="my-qr-reader">
+        </div>
+        <div class="modal fade" id="scanModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h1 class="modal-title fs-5" id="exampleModalLabel">Qr scan</h1>
-                        <button id="closeButtonId" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <button id="closeButtonId" type="button" class="btn-close" data-bs-dismiss="modal"
+                            aria-label="Close"></button>
                     </div>
                     <div class="modal-body mt-3 mb-3">
                         <p id="scanText"></p>
@@ -16,140 +17,140 @@
                 </div>
             </div>
         </div>
-      </div> 
-  </div> 
+    </div>
+</div>
 <script>
-function domReady(fn) { 
-    if ( 
-        document.readyState === "complete" || 
-        document.readyState === "interactive"
-    ) { 
-        setTimeout(fn, 1000); 
-    } else { 
-        document.addEventListener("DOMContentLoaded", fn); 
-    } 
-} 
-  
-domReady(function () { 
-    const scanModal = new bootstrap.Modal(document.getElementById("scanModal"), {});
-
-    var modal = document.getElementById("scanModal");
-    modal.addEventListener('click', function(){
-        let htmlscanner = new Html5QrcodeScanner( 
-        "my-qr-reader", 
-        { fps: 10, qrbos: 250 } 
-    ); 
-    htmlscanner.render(onScanSuccess); 
-    });
-
-    // If found you qr code 
-    function onScanSuccess(decodeText, decodeResult) { 
-        htmlscanner.clear();
-        scanModal.toggle();
-
-        requestData ={
-        action: "decrypt",
-        text: decodeText
+    function domReady(fn) {
+        if (
+            document.readyState === "complete" ||
+            document.readyState === "interactive"
+        ) {
+            setTimeout(fn, 1000);
+        } else {
+            document.addEventListener("DOMContentLoaded", fn);
         }
-
-        fetch("Components/encrypt.php", {
-        method: 'POST',
-        body: JSON.stringify(requestData),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-        })
-        .then(response => response.json())
-        .then(data => {
-            //var scanText = document.getElementById("scanText");
-            //scanText.innerHTML = data;
-            setModal(data);        
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-    }    
-
-    function setModal(data){
-         request = {
-        action: "getUserMenu",
-        userid: data.userid,
-        tod: data.tod,
-        date: data.date
-        } 
-        fetch("Components/getUserMenu.php", {
-        method: 'POST',
-        body: JSON.stringify(requestData),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-        })
-        .then(response => response.json())
-        .then(result => {
-            
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
     }
-  
-    let htmlscanner = new Html5QrcodeScanner( 
-        "my-qr-reader", 
-        { fps: 10, qrbos: 250 } 
-    ); 
-    htmlscanner.render(onScanSuccess); 
-});
+
+    domReady(function () {
+        const scanModal = new bootstrap.Modal(document.getElementById("scanModal"), {});
+
+        var modal = document.getElementById("scanModal");
+        modal.addEventListener('click', function () {
+            let htmlscanner = new Html5QrcodeScanner(
+                "my-qr-reader",
+                { fps: 10, qrbos: 250 }
+            );
+            htmlscanner.render(onScanSuccess);
+        });
+
+        // If found you qr code 
+        function onScanSuccess(decodeText, decodeResult) {
+            htmlscanner.clear();
+            scanModal.toggle();
+
+            requestData = {
+                action: "decrypt",
+                text: decodeText
+            }
+
+            fetch("Components/encrypt.php", {
+                method: 'POST',
+                body: JSON.stringify(requestData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(data => {
+                    //var scanText = document.getElementById("scanText");
+                    //scanText.innerHTML = data;
+                    setModal(data);
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        }
+
+        function setModal(data) {
+            request = {
+                action: "getUserMenu",
+                userid: data["userid"],
+                tod: data["tod"],
+                date: data["date"]
+            }
+            fetch("Components/getUserMenu.php", {
+                method: 'POST',
+                body: JSON.stringify(requestData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(result => {
+
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        }
+
+        let htmlscanner = new Html5QrcodeScanner(
+            "my-qr-reader",
+            { fps: 10, qrbos: 250 }
+        );
+        htmlscanner.render(onScanSuccess);
+    });
 </script>
 <style>
-.section { 
-    background-color: #ffffff; 
-    padding: 50px 30px; 
-    border: 1.5px solid #b2b2b2; 
-    border-radius: 0.25em; 
-    box-shadow: 0 20px 25px rgba(0, 0, 0, 0.25); 
-} 
-  
-#my-qr-reader { 
-    padding: 20px !important; 
-    border: 1.5px solid #b2b2b2 !important; 
-    border-radius: 8px; 
-} 
-  
-#my-qr-reader img[alt="Info icon"] { 
-    display: none; 
-} 
-  
-#my-qr-reader img[alt="Camera based scan"] { 
-    width: 100px !important; 
-    height: 100px !important; 
-} 
-  
-button { 
-    padding: 10px 20px; 
-    border: 1px solid #b2b2b2; 
-    outline: none; 
-    border-radius: 0.25em; 
-    color: white; 
-    font-size: 15px; 
-    cursor: pointer; 
-    margin-top: 15px; 
-    margin-bottom: 10px; 
-    background-color: #008000ad; 
-    transition: 0.3s background-color; 
-} 
-  
-button:hover { 
-    background-color: #008000; 
-} 
-  
-#html5-qrcode-anchor-scan-type-change { 
-    text-decoration: none !important; 
-    color: #1d9bf0; 
-} 
-  
-video { 
-    width: 100% !important; 
-    border: 1px solid #b2b2b2 !important; 
-    border-radius: 0.25em; 
-}
+    .section {
+        background-color: #ffffff;
+        padding: 50px 30px;
+        border: 1.5px solid #b2b2b2;
+        border-radius: 0.25em;
+        box-shadow: 0 20px 25px rgba(0, 0, 0, 0.25);
+    }
+
+    #my-qr-reader {
+        padding: 20px !important;
+        border: 1.5px solid #b2b2b2 !important;
+        border-radius: 8px;
+    }
+
+    #my-qr-reader img[alt="Info icon"] {
+        display: none;
+    }
+
+    #my-qr-reader img[alt="Camera based scan"] {
+        width: 100px !important;
+        height: 100px !important;
+    }
+
+    button {
+        padding: 10px 20px;
+        border: 1px solid #b2b2b2;
+        outline: none;
+        border-radius: 0.25em;
+        color: white;
+        font-size: 15px;
+        cursor: pointer;
+        margin-top: 15px;
+        margin-bottom: 10px;
+        background-color: #008000ad;
+        transition: 0.3s background-color;
+    }
+
+    button:hover {
+        background-color: #008000;
+    }
+
+    #html5-qrcode-anchor-scan-type-change {
+        text-decoration: none !important;
+        color: #1d9bf0;
+    }
+
+    video {
+        width: 100% !important;
+        border: 1px solid #b2b2b2 !important;
+        border-radius: 0.25em;
+    }
 </style>
