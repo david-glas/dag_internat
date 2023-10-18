7a64027845cad25a5b5f948dba3d94b34b16d75a
David Glas <david@glas.at>
2023-10-18 20:16:50 +0200

    overview erweiterung

---
+++ Page/Components/overview_functions.php
@@ -0,0 +1,32 @@
+<?php
+include "../Database/conn.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $test = $_POST;
+  $action = "";
+  $values = "";
+  $set_time = false;
+
+  if (isset($_POST['delete'])) {
+    $action = 'delete';
+    $values = $_POST['delete'];
+  }
+  else if (isset($_POST['add'])) {
+    $action = 'add';
+    $values = $_POST['add'];
+    if(isset($_POST['set_time'])) { $set_time = true; }
+  }
+
+  list($user_id, $menu_id) = explode('_', $values);
+
+  $menu = new Menu();
+  if ($action == 'add' && $user_id != -1) {
+    $menu->AddUserToMenu($menu_id, $user_id, $set_time);
+  }
+  else if ($action == 'delete') {
+    $menu->RemoveUserFromMenu($menu_id, $user_id);
+  }
+}
+
+  header("Location: ../index.php?page=dashboard");
+?>
--- Page/Pages/overview.php
+++ Page/Pages/overview.php
@@ -11,6 +11,7 @@
           <th scope="col">Essen ID</th>
           <th scope="col">Essensname</th>
           <th scope="col">Anzahl</th>
+          <th scope="col"></th>
         </tr>
       </thead>
       <tbody>
@@ -27,6 +28,8 @@ makeChartScript();
 function fillMenuData() {
   $menuConn = new Menu();
   $menus = $menuConn->GetMenuForWeek();
+  $userConn = new User();
+  $allUsers = $userConn->GetAllUsers('where role_id in (2,3)');
 
   $lastDay = '';
 
@@ -40,7 +43,12 @@ function fillMenuData() {
           <td>'.$menu['type'].'</td>  
           <td>'.$menu['food_id'].'</td>
           <td>'.$menu['name'].'</td>
-          <th>'.$menu['amount'].'</td>
+          <th>'.$menu['amount'].'</th>
+          <td>
+            <button type="button" class="btn btn-warning btn-sm float-end" data-bs-toggle="modal" data-bs-target="#show'.$menu['menu_id'].'">
+              <i class="bi bi-display"></i><span>  Anzeigen</span>
+            </button>
+          </td>
         </tr>';
       }
       else {
@@ -49,14 +57,88 @@ function fillMenuData() {
           <td>'.$menu['type'].'</td>  
           <td>'.$menu['food_id'].'</td>
           <td>'.$menu['name'].'</td>
-          <th>'.$menu['amount'].'</td>
+          <th>'.$menu['amount'].'</th>
+          <td>
+            <button type="button" class="btn btn-warning btn-sm float-end" data-bs-toggle="modal" data-bs-target="#show'.$menu['menu_id'].'">
+              <i class="bi bi-display"></i><span>  Anzeigen</span>
+            </button>
+          </td>
         </tr>';
       }
+      echo
+      '<div class="modal fade" id="show'.$menu['menu_id'].'">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">'.$menu['day'].', '.$menu['day_name'].' - '.$menu['type'].'</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <div class="container-fluid">
+                <div class="row mb-3">
+                  <div class="col-2"></div>
+                  <div class="col-4"><b>Name</b></div>
+                  <div class="col-6"><b>Abgeholt</b></div>
+                </div>';
+                printUsersByMenuID($userConn, $menu['menu_id']);
+        echo    '
+              </div>
+              <hr class="divider">
+              <div class="container-fluid">
+                <div class="row">
+                  <form method="POST" action="Components/overview_functions.php">
+                    <div class="col-2">
+                      <button type="submit" name="button" value="add" class="btn btn-sm btn-success"><i class="bi bi-plus-lg"></i></button>
+                    </div>
+                    <div class="col-4">
+                      <select class="form-select form-select-sm" name="add">
+                        <option value="-1_-1" selected>Name</option>';
+                        foreach ($allUsers as $user) {
+                          echo '<option value="'.$user['user_id'].'_'.$menu['menu_id'].'">'.$user['firstname'].' '.$user['lastname'].'</option>';
+                        }
+        echo          '</select>
+                    </div>
+                    <div class="col-6">
+                      <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" role="switch" name="set_time">
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>';
+
       $lastDay = $menu['day'];
     }
   }
 }
-
+function printUsersByMenuID(User $userConn, $menu_id) {
+  $users = $userConn->GetUsersByMenuID($menu_id);
+  
+  foreach ($users as $user) {
+    echo
+    '<div class="row mt-1">
+      <form method="POST" action="Components/overview_functions.php">
+        <div class="col-2">
+          <button type="submit" name="delete" value="'.$user['user_id'].'_'.$menu_id.'" class="btn btn-sm btn-danger"><i class="bi bi-x"></i></button>
+        </div>
+      </form>
+      <div class="col-4">'.$user['name'].'</div>
+      <div class="col-6">'.printZusage($user['time']).'</div>
+    </div>';
+  }
+}
+function printZusage($time) : string {
+  if ($time == null) {
+    return "Nein";
+  }
+  else {
+    return $time;
+  }
+}
 function makeChartScript() {
   $menuConn = new Menu();
   $menus = $menuConn->GetMenuForWeek();
