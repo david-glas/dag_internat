0bc2cae48cc9b024592d0c1fb1803cebd7fd70d2
Adnan Mehic <adnan.mehic@students.bs-linz2.ac.at>
2023-10-18 11:46:11 +0200

    splitted instructions pages

---
+++ Page/Pages/instructionswebuser.php
@@ -0,0 +1,17 @@
+<div class="container mt-5 mr-3 ml-3">
+
+  <hr class="featurette-divider">
+
+  <h1 class="display-3">Instructions</h1>
+  
+  <hr class="featurette-divider">
+  
+  <p class="fs-5">
+    Willkommen zur Anleitung für die Nutzung unserer Webapplikation zur Verwaltung 
+    der Verpflegung für Internatsschüler. Diese Anleitung erklärt, wie verschiedene 
+    Benutzergruppen die Funktionen der Anwendung nutzen können.
+  </p>
+  <h4>
+    Für eine detailierte Anleitung zur Nutzung der Website loggen Sie sich bitte mit Ihrem Benutzer ein.
+  </h4>
+</div>
